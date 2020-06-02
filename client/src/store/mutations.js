e751fb5d369d722513d497be889677e5843d61a3
Florent <flgallois@gmail.com>
2020-06-03 00:13:58 +0200

    change mutation

--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -1,13 +1,14 @@
 const mutations = {
     INIT: (state) => {
-      const id = localStorage.getItem('id')
-      const token = localStorage.getItem('token')
-      if (token && id) {
-        state.id = id
-        state.token = token
         state.authentificated = true
-        // axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
-      }
+    //   const id = localStorage.getItem('id')
+    //   const token = localStorage.getItem('token')
+    //   if (token && id) {
+    //     state.id = id
+    //     state.token = token
+    //     state.authentificated = true
+    //     // axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
+    //   }
     }
 }
 
